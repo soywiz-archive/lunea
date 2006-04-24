10e9416d488b6293416333777b03b98d7b181068
soywiz <soywiz@a9bc85d4-af10-0410-a9bb-bcf6533f7dc0>
2006-04-24 18:58:58 +0000

    git-svn-id: svn://svn.code.sf.net/p/lunea/code@4 a9bc85d4-af10-0410-a9bb-bcf6533f7dc0

--- trunk/install/install.bat
+++ trunk/install/install.bat
@@ -1,30 +1,46 @@
 @echo off
 
+SET SUCCESS=1
+
+SET RETERR=reterr1
 @dir /D dm*c.zip > NUL 2> NUL
 IF NOT %ERRORLEVEL%==0 GOTO notex1
 
+:reterr1
+
+SET RETERR=reterr2
 @dir /D bup.zip > NUL 2> NUL
 IF NOT %ERRORLEVEL%==0 GOTO notex2
 
+:reterr2
+
+SET RETERR=reterr3
 @dir /D dmd*.zip > NUL 2> NUL
 IF NOT %ERRORLEVEL%==0 GOTO notex3
+
+:reterr3
+
+IF %SUCCESS%==0 GOTO insterroend
 
 GOTO install
 
 :notex1
-echo NO EXISTE EL FICHERO "dm*c.zip"
+echo NO EXISTE EL FICHERO "dm*c.zip" ( Digital Mars C/C++ Compiler Version 8.47 )
 echo DEBE BAJARLO DE http://www.digitalmars.com/download/dmcpp.html
-GOTO insterroend
+SET SUCCESS=0
+GOTO %RETERR%
 
 :notex2
-echo NO EXISTE EL FICHERO "bup.zip"
+echo NO EXISTE EL FICHERO "bup.zip" ( Basic Utilities )
 echo DEBE BAJARLO DE http://www.digitalmars.com/download/dmcpp.html
-GOTO insterroend
+SET SUCCESS=0
+GOTO %RETERR%
 
 :notex3
-echo NO EXISTE EL FICHERO "dmd*.zip"
+echo NO EXISTE EL FICHERO "dmd*.zip" ( Digital Mars D Compiler )
 echo DEBE BAJARLO DE http://www.digitalmars.com/d/changelog.html
-GOTO insterroend
+SET SUCCESS=0
+GOTO %RETERR%
 
 :install
 echo Instalando...
@@ -37,16 +53,26 @@ IF NOT %ERRORLEVEL%==0 GOTO insterror
 echo Descomprimiendo bup.zip...
 unzip -u -o bup.zip  -d ..\bin > NUL 2> NUL
 IF NOT %ERRORLEVEL%==0 GOTO insterror
-echo Descomprimiendo phobos_patch.7z...
 cd ..\bin\dmd\src
+
+IF NOT EXIST "..\..\..\install\phobos_patch.7z" GOTO mod_src
+
+echo Descomprimiendo phobos_patch.7z...
 ..\..\..\install\7z x -y -bd ..\..\..\install\phobos_patch.7z > NUL 2> NUL
 IF NOT %ERRORLEVEL%==0 GOTO insterror
-echo Modificando el directorio SRC...
+
+:mod_src
+
+IF NOT EXIST "..\..\..\install\src" GOTO finish_install
+
+echo Copiando cambios del directorio SRC...
 XCOPY /S /Q /Y /I ..\..\..\install\src . > NUL 2> NUL
 echo Compilando librería PHOBOS.LIB...
 @CALL makelib.bat > NUL
 cd ..\..\..\install
 IF NOT %ERRORLEVEL%==0 GOTO insterror
+
+:finish_install
 
 echo Instalacion completada satisfactoriamente
 GOTO end
